2b9e8c70678ce13abee057e5b74a7b092a952df6
Deymdeej <charlottegaliacc@gmail.com>
2024-08-29 17:44:30 +0800

    descriptions

--- src/content_option.js
+++ src/content_option.js
@@ -1,39 +1,35 @@
 const logotext = "JOHN";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "John Rey Galia",
+    description: "I’m John Rey Galia a 4th Year IT student",
 };
 
 const introdata = {
     title: "I’m John Rey",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "Very Mindful",
+        second: "Very Demure",
+        third: "Very Cutesy",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "with a deep passion for technology, eager to apply my knowledge of IT processes and challenges in a professional setting",
+    your_img_url: "C:\\Users\\John Rey Galia\\Desktop\\JRG.JPG"
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "ABOUT ME",
+    aboutme: "Hi! I am John Rey, a motivated 4th-year IT student with a deep passion for technology, eager to apply my knowledge of IT processes and challenges in a professional setting. I possess strong troubleshooting and communication skills and have a proven ability to collaborate effectively with stakeholders to enhance operations and achieve organizational goals. I am seeking an internship opportunity where I can utilize my technical insights and expertise to contribute to ongoing IT projects and help develop long-term strategies that drive success.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: " ",
+        where: " ",
+        date: " ",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
+        jobtitle: "Remotask.com",
+        where: "Data Labeling & Annotation Tasker",
+        date: "MAR 2020 – JUL 2020",
+    }
+    
 ];
 
 const skills = [{
@@ -136,21 +132,14 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    YOUR_EMAIL: "galiajohnrey01@gmail.com",
+    YOUR_FONE: "+63 976 086 1466",
+    description: "Thank you for passing by. I look forward to the possibility of contributing to the innovative work! ",
 };
 
 const socialprofils = {
     github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    facebook: "https://facebook.com"
 };
 export {
     meta,
